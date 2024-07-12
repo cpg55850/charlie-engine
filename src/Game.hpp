55f3722408c7479513cade667e332daed7dd15a9
Charlie Graham <charliechip95@gmail.com>
2024-07-11 20:22:35 -0500

    Initial commit

---
+++ src/Game.hpp
@@ -0,0 +1,32 @@
+/*This source code copyrighted by Lazy Foo' Productions 2004-2024
+and may not be redistributed without written permission.*/
+
+// Using SDL, SDL_image, SDL_ttf, standard IO, strings, and string streams
+#include <SDL.h>
+// #include <SDL_image.h>
+// #include <SDL_ttf.h>
+#include <stdio.h>
+
+// #include <sstream>
+// #include <string>
+
+class Game {
+ public:
+  Game();
+  ~Game();
+
+  void init(const char *title, int xpos, int ypos, int width, int height,
+            bool fullscreen);
+
+  void handleEvents();
+  void update();
+  void render();
+  void clean();
+
+  bool running() { return isRunning; }
+
+ private:
+  bool isRunning;
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+};
